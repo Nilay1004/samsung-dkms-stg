ef97308ff842a9e8d4c00c47938efb87b254ad0c
Nilay1004 <nilaykujur@gmail.com>
2024-08-22 07:59:13 +0000

    Delete 20240624121212_add_hashed_email_to_user_emails.rb

--- db/migrate/20240624121212_add_hashed_email_to_user_emails.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# Adds a new column hashed_email to the user_emails table for storing hashed email data.
-
-class AddHashedEmailToUserEmails < ActiveRecord::Migration[6.1]
-  def change
-    add_column :user_emails, :hashed_email, :string
-  end
-end
